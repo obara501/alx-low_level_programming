cb38aee4d4688db8abc62bc664288baf3e84de89
obara501 <geofreyongeta501@gmail.com>
2023-10-05 23:33:54 +0000

    print statement with printf() function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.>
+/**
+ * main - print statement with printf()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but we outcome is a piece of art,\n");
+	return (0);
+}
