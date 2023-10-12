bc6c9120761a3ba3b27f23cfee4d8d7513e58a98
obara501 <geofreyongeta501@gmail.com>
2023-10-12 19:03:50 +0000

    add two int and return sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Enty point of application.
+ *
+ * Description: Runs a program to print the sum of two integers.
+ * Return: 0 (Success)
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
