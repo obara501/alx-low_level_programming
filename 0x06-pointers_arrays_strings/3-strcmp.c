16ecb23e0375f360c02a6077deddde3c03d6a9b3
obara501 <geofreyongeta501@gmail.com>
2023-10-19 14:20:17 +0000

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings in lexical order.
+ * @s1: The first string
+ * @s2: The second string.
+ *
+ * Return: 0 if the strings are equal, otherwise the lexical (alphabetical)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+			continue;
+		}
+		break;
+	}
+	return (*s1 - *s2);
+}
