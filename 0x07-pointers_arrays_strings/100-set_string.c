17689dc38d23d5faeb3504880bffae61a6f2da26
Geofrey (J3ff) Obara <geofreyongeta501@gmail.com>
2023-10-24 21:23:50 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to change
- * @to: string to change pointer to
- *
- * Return: void
- */
+  * set_string - main function
+  * @s: Function parameter
+  * @to: Function parameter
+  *
+  * Return: Always 0.
+  */
 void set_string(char **s, char *to)
+
 {
 	*s = to;
 }
+
