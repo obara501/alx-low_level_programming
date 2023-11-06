4a4a668bae9bd2498fb6e64b0d2801be3aaaf6e0
Geofrey (J3ff) Obara <geofreyongeta501@gmail.com>
2023-11-06 22:56:19 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog - Structure of a dog
+* @name: - Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
