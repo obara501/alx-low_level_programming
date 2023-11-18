4a5134250a34a9d166cc518e8e1eab46857728ca
obara501 <geofreyongeta501@gmail.com>
2023-11-18 11:32:11 +0000

    update  4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
-
-	while (head)
+	if (head)
 	{
-		temp = head->next;
-		free(head->str);
+		free_list(head->next);
+
+		if (head->str)
+			free(head->str);
+
 		free(head);
-		head = temp;
 	}
 }
