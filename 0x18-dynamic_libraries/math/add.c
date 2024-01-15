de9d548554b40b57180a2846e880a0ca9b1ca523
Geofrey (J3ff) Obara <geofreyongeta501@gmail.com>
2024-01-15 09:57:48 +0300

    create add.c

---
+++ 0x18-dynamic_libraries/math/add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - Adds two integers
+* @a: First number
+* @b: Second number
+*
+* Return: The sum of arguments a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
